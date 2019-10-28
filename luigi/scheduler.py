08a12b08c0849c7e7d6ae9f70e902143202276d6
Sam Millar <sammillar@mixcloud.com>
2019-10-28 16:52:35 +0000

    Suppress warning

--- luigi/scheduler.py
+++ luigi/scheduler.py
@@ -95,7 +95,7 @@ def rpc_method(**request_args):
     def _rpc_method(fn):
         # If request args are passed, return this function again for use as
         # the decorator function with the request args attached.
-        fn_args = inspect.getargspec(fn)
+        fn_args = six.getargspec(fn)
 
         assert not fn_args.varargs
         assert fn_args.args[0] == 'self'
